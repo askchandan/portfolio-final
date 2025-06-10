eb7f54f9081b5f16182fef8404c9a232f09697f5
askchandan <chandanmalakar6209@gmail.com>
2025-06-11 00:03:17 +0530

    Final1

--- next.config.js
+++ next.config.js
@@ -1,7 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-};
+  output: 'export',
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
+  poweredByHeader: false,
+  // Minimal experimental config to avoid issues
+  experimental: {
+    // Disable potentially problematic optimizations
+    optimizePackageImports: [],
+  },
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
